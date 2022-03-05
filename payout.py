250fb853959cf1af7d82e8ddc96c21873c527670
Christopher Campbell <campbellcl@mac.com>
2022-03-04 20:24:45 -0500

    Tweaked formulas in script.js and payout.py

--- payout.py
+++ payout.py
@@ -4,12 +4,12 @@ stake = int(input('How much do you intend to bet?: '))
 if odds > 0:
 	odds_multiplier = odds/100
 	implied_prob = round(100/(odds + 100), 2)
-	payout = round(stake * odds_multiplier,2)
-	print(str(f'The implied probability of winning the bet is {implied_prob * 100}%'))
-	print(str(f'Your potential payout (profit + stake) is: {payout + stake}'))
+	profit = round(stake * odds_multiplier,2)
+	print(str(f'The implied probability of winning the bet is {round(implied_prob * 100, 2)}%'))
+	print(str(f'Your potential payout (profit + stake) is: {round(profit + stake, 2)}'))
 elif odds < 0:
 	odds_multiplier = abs(odds/100)
 	implied_prob = round(abs(odds)/(abs(odds) + 100), 2)
-	payout = round(stake / odds_multiplier,2)
-	print(str(f'The implied probability of winning the bet is {implied_prob * 100}%'))
-	print(str(f'Your potential payout (profit + stake) is: {payout + stake}'))
+	profit = round(stake / odds_multiplier,2)
+	print(str(f'The implied probability of winning the bet is {round(implied_prob * 100, 2)}%'))
+	print(str(f'Your potential payout (profit + stake) is: {round(profit + stake, 2)}'))
--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 // Get user input on the odds and how much is being bet (this is for use in Node JS):
 const prompt = require('prompt-sync')();
 
-var odds = prompt('What are the odds of your bet?: ');
-var stake = prompt('How much do you intend to bet?: ');
+let odds = prompt('What are the odds of your bet?: ');
+odds = parseInt(odds);
+let stake = prompt('How much do you intend to bet?: ');
+stake = parseInt(stake);
 
 // Function for determining the probability and payout:
 
 const odds_payout = (odds, stake) => {
     if (odds > 0) {
         odds_multiplier = odds/100;
-        implied_prob = Math.round(Math.abs(100/(odds + 100)));
-        payout = Math.round(stake * odds_multiplier);
-        console.log(`The implied probability of winning the bet is ${implied_prob}%`);
-        console.log(`Your potential payout (profit + stake) is: ${payout + stake}`)
-    } else if (odds < 0) {
-        odds_multiplier = Math.abs(odds/100);
-        implied_prob = Math.round(Math.abs(odds)/Math.abs(odds) + 100);
-        payout = Math.round(stake / odds_multiplier);
-        console.log(`The implied probability of winning the bet is ${implied_prob * 100}%`);
-        console.log(`Your potential payout (profit + stake) is: ${payout + stake}`)
+        implied_prob = (100/(odds + 100) * 100).toFixed(2);
+        profit = (stake * odds_multiplier).toFixed(3);
+        console.log('The implied probability of winning the bet is ' + implied_prob + '%');
+        console.log('Your potential profit is: ' + profit);
+    } else if (odds < 0) {        
+        odds = Math.abs(odds);
+        odds_multiplier = odds/100;
+        implied_prob = (odds/(odds + 100) * 100).toFixed(2);
+        profit = (stake * odds_multiplier).toFixed(3);
+        console.log('The implied probability of winning the bet is ' + implied_prob + '%');
+        console.log('Your potential profit is: ' + profit);
     }
     
 }
